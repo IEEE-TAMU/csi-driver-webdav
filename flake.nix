b1622b33abd06994890d20609425c0bfe57226a8
Caleb Norton <n0603919@outlook.com>
2025-02-02 21:49:03 -0600

    fixup

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
         };
 
-        docker = pkgs.dockerTools.buildLayeredImage {
+        docker = pkgs.dockerTools.buildImage {
           name = "webdavplugin";
           tag = "latest";
           contents = pkgs.buildEnv {
@@ -37,9 +37,17 @@
             paths = with pkgs; [
               mount
               davfs2
-              fakeNss # https://nixos.org/manual/nixpkgs/stable/#ssec-pkgs-dockerTools-shadowSetup
             ];
           };
+          # https://nixos.org/manual/nixpkgs/stable/#ssec-pkgs-dockerTools-shadowSetup
+          runAsRoot = ''
+            ${pkgs.dockerTools.shadowSetup}
+            groupadd -r davfs2
+            useradd -r -g davfs2 davfs2
+            ln -s /proc/mounts /etc/mtab # davfs2 needs this
+            mkdir -p /var/cache/davfs2
+            chmod 0600 /etc/davfs2/secrets # reset permissions
+          '';
           config.Entrypoint = ["${webdavplugin}/bin/webdav"];
         };
       };
